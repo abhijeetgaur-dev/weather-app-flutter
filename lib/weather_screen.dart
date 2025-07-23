a83df7ecc343b9a902d4605d775aef35ca9fc591
Abhijeet Gaur <abhijeetgaur.dev@gmail.com>
2025-07-24 02:42:47 +0530

    completed the ui of the app

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:weather_app/hourly_forecast_item.dart';
+import 'package:weather_app/weather_info.dart';
 
 class WeatherScreen extends StatelessWidget {
   const WeatherScreen({super.key});
@@ -24,34 +28,98 @@ class WeatherScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             //main card
             SizedBox(
               width: double.infinity,
               child: Card(
-                child: Column(
-                  children: [
-                    Text(
-                      '300 K',
-                      style: TextStyle(
-                        fontSize: 32,
-                        fontWeight: FontWeight.bold,
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(16),
+                  child: BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: [
+                          Text(
+                            '300 K',
+                            style: TextStyle(
+                              fontSize: 32,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 20),
+                          Icon(Icons.cloud, size: 64),
+                          const SizedBox(height: 20),
+                          Text('Rain', style: TextStyle(fontSize: 20)),
+                        ],
                       ),
                     ),
-                    const SizedBox(height: 20),
-                    Icon(Icons.cloud, size: 64),
-                    const SizedBox(height: 20),
-                    Text('Rain', style: TextStyle(fontSize: 20)),
-                  ],
+                  ),
                 ),
               ),
             ),
             const SizedBox(height: 20),
             //weather forecast card
-            const Placeholder(fallbackHeight: 150),
+            Text(
+              'Weather Forecast',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 16),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  HourlyForecastItem(
+                    time: "09:10",
+                    temperature: "300K",
+                    icon: Icons.cloud,
+                  ),
+                  HourlyForecastItem(
+                    time: "10:30",
+                    temperature: "290K",
+                    icon: Icons.cloud,
+                  ),
+                  HourlyForecastItem(
+                    time: "02:40",
+                    temperature: "280K",
+                    icon: Icons.sunny,
+                  ),
+                  HourlyForecastItem(
+                    time: "10:10",
+                    temperature: "270K",
+                    icon: Icons.cloud,
+                  ),
+                ],
+              ),
+            ),
             const SizedBox(height: 20),
             //additional information
-            const Placeholder(fallbackHeight: 150),
+            Text(
+              'Additional Information',
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                WeatherInfo(
+                  icon: Icons.water_drop,
+                  label: 'Wind Speed',
+                  value: '7.5',
+                ),
+                WeatherInfo(icon: Icons.air, label: 'Humidity', value: '91'),
+                WeatherInfo(
+                  icon: Icons.beach_access,
+                  label: 'Pressure',
+                  value: '1014',
+                ),
+              ],
+            ),
           ],
         ),
       ),
